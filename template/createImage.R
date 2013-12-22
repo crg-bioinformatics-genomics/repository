c90ff57d8970cd267c82b6790b6b8814f55f3f66
Federico <agostini.federico@gmail.com>
2013-12-22 08:05:32 +0100

    few changes

--- template/createImage.R
+++ template/createImage.R
@@ -12,8 +12,10 @@ RRMlen=as.numeric(args[7])
 
 data=read.table("./outputs/RBP.out")
 title="RBPprofile scan"
-png("./outputs/image.png",width=1920,height=480,pointsize=20)
-#png("./outputs/image.png",width=750,height=159)
+
+# HD image
+png("./outputs/HD_image.png",width=1920,height=480,pointsize=20)
+#png("./outputs/image.png",width=750,height=160)
 
 #newdata=rbind(zerovector,data)
 #bla=rbind(zerovector,halfdata)
@@ -43,7 +45,37 @@ if (score>0.62){
 
 dev.off()
 
+# NORMAL image
+# png("./outputs/image.png",width=1920,height=480,pointsize=20)
+png("./outputs/image.png",width=750,height=160)
 
+#newdata=rbind(zerovector,data)
+#bla=rbind(zerovector,halfdata)
+
+if (score>0.62){
+	subtitle=paste("score: ",score,"    significant binding region found (",domain,"*)",sep="")
+	image.plot(as.matrix(t(data)),frame.plot=F,yaxt='n',xaxt='n',col=colorpanel(10,"blue","white","red"), zlim=c(-1, 1),horizontal=T,legend.width=1,legend.shrink=0.2,	
+	smallplot=c(.7,.85,.3,.35),bigplot=c(.1,.9,.6,.7),main=title,sub=subtitle)
+	image(as.matrix(t(data)),frame.plot=F,yaxt='n',xaxt='n',col=colorpanel(10,"blue","white","red"), zlim=c(-1, 1),ad=TRUE)
+#	axis(1,at=seq(0,1,0.1),labels=seq(0,length(data),as.integer(length(data)/10)),lwd=0,lwd.ticks=1)
+	#axis(1,at=seq(0,1,0.1),labels=seq(1,length(data),as.integer(length(data)/10)),lwd=0,lwd.ticks=1)
+	axis(1,at=seq(0,1,0.1),labels=seq(as.integer(RRMlen/2),as.integer(length(data)+(RRMlen/2)),as.integer(length(data)/10)),lwd=0,lwd.ticks=1)
+#	axis(1,at=seq(0,1.1,0.1),labels=seq(1,as.integer(length(data)+(RRMlen/2)),as.integer(length(data)/10)),lwd=0,lwd.ticks=1)
+
+	}else{
+	new=data*0.62
+	subtitle=paste("low score:    NO significant binding region found (",domain,"*)",sep="")
+	image.plot(as.matrix(t(new)),frame.plot=F,yaxt='n',xaxt='n',col=colorpanel(10,"blue","white","red"), zlim=c(-1, 1),horizontal=T,legend.width=1,legend.shrink=0.2,	
+	smallplot=c(.7,.85,.3,.35),bigplot=c(.1,.9,.6,.7),main=title,sub=subtitle)
+	image(as.matrix(t(new)),frame.plot=F,yaxt='n',xaxt='n',col=colorpanel(10,"blue","white","red"), zlim=c(-1, 1),ad=TRUE)
+#	axis(1,at=seq(0,1,0.1),labels=seq(1,length(new),as.integer(length(new)/10)),lwd=0,lwd.ticks=1)
+	axis(1,at=seq(0,1,0.1),labels=seq(as.integer(RRMlen/2),as.integer(length(data)+(RRMlen/2)),as.integer(length(data)/10)),lwd=0,lwd.ticks=1)
+
+}
+
+
+
+dev.off()
 
 #cat heatmap.R | R --slave --args score domain
 #library(gplots)
